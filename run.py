b082d57ac0fc75be60523b6a156b56d94fb91def
Johan van Soest <info@johanvansoest.nl>
2022-08-17 21:01:18 +0200

    Get repository paths for certain groups

--- run.py
+++ run.py
@@ -6,4 +6,11 @@ for group in groups:
     visibility = group.attributes['visibility']
     full_path = group.attributes['full_path']
     description = group.attributes['description']
-    
+    if not full_path.startswith("medicaldataworks"):
+        continue
+    myGroup = gl.groups.get(group.attributes['id'])
+    projects = myGroup.projects.list()
+    for x in projects:
+        project = gl.projects.get(x.attributes["id"])
+        print(x.attributes["ssh_url_to_repo"])
+        print(x)
